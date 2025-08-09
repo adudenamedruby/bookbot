79950883ecf73a252a1c353d02d819d5025ecfc9
roux g. buciu <git@adudenamedruby.com>
2025-08-09 08:55:42 -0400

    update with sys

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from stats import character_counter_for
 from stats import count_words_in
 from stats import split_dict
 
+import sys
+
 
 def pretty_print(filepath):
     count = count_words_in(filepath)
-    dict = character_counter_for("./books/frankenstein.txt")
+    dict = character_counter_for(filepath)
     count_string = ""
     for item in split_dict(dict):
         count_string += f"{item['char']}: {item['num']}\n"
@@ -23,7 +25,11 @@ def pretty_print(filepath):
 
 
 def main():
-    print(pretty_print("books/frankenstein.txt"))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    print(pretty_print(filepath))
 
 
 main()
